726176b8da1a9395bbb679d1233ac33b11c14365
jasongodfrey6963 <35274334+jasongodfrey6963@users.noreply.github.com>
2019-05-16 13:20:31 -0500

    Update Toolbar.vue

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,8 +1,8 @@
 <template>
   <v-toolbar v:bind:location="location">
       <v-toolbar-title class="headline text-uppercase">
-        <span>Domiblows </span>
-        <span class="font-weight-light">PIZZA</span>
+        <span>PIZZA </span>
+        <span class="font-weight-light">RESTAURANT</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn flat @click="openOrder" class="mx-0 px-0"><span>COUNTER</span></v-btn>
